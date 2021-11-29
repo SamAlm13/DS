b11ae73028681a653d8de1feaca9bc9616669299
gustavo-gabriel-OL <gustavogbriel2004@gmail.com>
2021-11-29 11:58:21 -0300

    acabo

--- aluguel/index.php
+++ aluguel/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="public\css\bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 
     <title>Carrinho de compras</title>   
@@ -21,7 +21,7 @@
   <li><a href="cadastrocliente.html">Cadastro de clientes</a></li>
   <li><a href="cadastrofuncionario.html">Cadastro de funcionários</a></li>
   <li><a href="cadastroveiculo.html">Cadastro de veículos</a></li>
-  <li style="float:right"><a class="active" href="#about">login</a></li>
+  <li style="float:right"><a class="active" href="login.html">login</a></li>
 </ul>
 
 <div style="overflow:auto">
@@ -33,10 +33,10 @@
   </div>
 
   <div class="main">
-    <h2>Vitrine Virtual</h2>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+    <h1>Vitrine Virtual</h1>
+    <h2>Venha para a nossa locadora, aqui nós temos os melhores carros para alugar, pelo melhor preço!!</h2>
 
-        <div class="row row-cols-1 row-cols-md-3">
+    <div class="row row-cols-1 row-cols-md-3">
         <?php $conn=mysqli_connect("localhost", "root", "","carrinho");     
         $sql = "SELECT * FROM produtos";
         $qr = mysqli_query($conn,$sql) or die(mysqli_error()); 
@@ -45,35 +45,25 @@
         while($ln = mysqli_fetch_assoc($qr)){ ?>
           <div class="col mb-4">
             <div class="card h-100">
-              <img src="..." class="card-img-top" alt="...">
+              <img src="<?php echo 'img/'.$ln['imagem']; ?> " class="card-img-top" alt="...">
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title"><?php echo '<h2>'.$ln['nome'].'</h2> <br />';?></h5>
+                <p class="card-text"><?php echo 'Preço : R$ '.number_format($ln['preco'], 2, ',', '.').'<br />';?></p>
+                <a href="carrinho.php?acao=add&id=<?php echo $ln['id']; ?>"class="btn btn-primary">Locar</a>
               </div>
             </div>
           </div>
-          
-          <?php } ?>
-        </div>
-
-    <?php
-          
-          while($ln = mysqli_fetch_assoc($qr)){
-             echo '<h2>'.$ln['nome'].'</h2> <br />';
-             echo 'Preço : R$ '.number_format($ln['preco'], 2, ',', '.').'<br />';
-             echo '<img src="img/'.$ln['imagem'].'" /> <br />';
-             echo '<a href="carrinho.php?acao=add&id='.$ln['id'].'">Comprar</a>';
-             echo '<br /><hr />';
-          }
-    ?>
+          <?php } 
+          echo '<br /><hr />';?>
+    </div>
   </div>
 
-  <div class="right">
+    <div class="right">
     <h2>Promoções</h2>
     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-  </div>
+    </div>
+ </div>
 </div>
-
 <div style="background-color:#e5e5e5;text-align:center;padding:10px;margin-top:7px;">© copyright from Gustavo Gabriel de Oliveira Lima</div>
 
 </body>
